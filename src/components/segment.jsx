331e9a8800b1b339c04cfe832b92490336951720
hptjohn <hptjohn@gmail.com>
2026-04-28 17:48:17 +0800

    Add user identification script in segment.jsx

---
+++ src/components/segment.jsx
@@ -0,0 +1,19 @@
+<script>
+ 	var newUserId = ' ' ;
+	newUserId=crypto.randomUUID();
+
+function identifyUser() {
+	var userId = ' ' ;
+	userId = newUserId;
+
+analytics.identify(‘userId’ , {
+	email : document.getElementById(‘email’).value,
+	phone : document.getElementById(‘phone’).value,
+	plan : document.getElementById(‘plan’).value,
+	petName : document.getElementById(‘pet’).value,
+	firstName: document.getElementById(‘firstName’).value,
+	lastName : document.getElementById(‘lastName’).value
+	});
+
+}
+</script>
